2dc287d1e7a0f2925ad874a266172b695d9326df
Pierre Parré <pierre@burotec40.fr>
2023-05-23 14:28:50 +0200

    feat(courses): add list of courses, and get courses detail endpoints

--- index.js
+++ index.js
@@ -3,5 +3,24 @@ const port = 9001;
 const express = require('express');
 const app = express();
 
+const courses = require('./courses.json');
+const reservations = require('./reservations.json');
+
 // use json middleware
 app.use(express.json());
+
+// list all courses
+app.get('/courses', (req, res) => {
+    res.status(200).json(courses);
+});
+
+// get details of a particular course
+app.get('/courses/:id', (req, res) => {
+    const course = courses.find(course => course.id === req.params.id);
+    if (course) {
+        res.status(200).json(course);
+    } else {
+        res.status(404).json({ message: 'Course not found' });
+    }
+});
+
